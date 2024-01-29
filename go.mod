005fbe2317593c9e3d03e3b491a776a4d883791d
unknown <ibeawuchiclaret@gmail.com>
2024-01-28 23:34:01 -0800

    setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Greyisheep/go-feeds
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
 
 }
