dd0b9183d9b701bbc5245b471e3be0101d86a923
unknown <ibeawuchiclaret@gmail.com>
2024-01-31 20:52:59 -0800

    update

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Greyisheep/go-feeds/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
@@ -22,6 +30,18 @@ func main() {
 	if dbURL == "" {
 		log.Fatal("DB_URL is not found in the environment")
 	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database", err)
+	}
+
+	db := database.New(conn)
+	apiCfg := apiConfig{
+		DB: db,
+	}
+
+	// go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -38,6 +58,7 @@ func main() {
 
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -47,7 +68,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port: %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
